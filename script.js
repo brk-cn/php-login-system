ac175935436243ac48e0a2387f26db74dd52adcb
burak can <brkcndev@gmail.com>
2024-01-10 20:25:00 +0300

    form design

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSignUpForm() {
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("sign-up-form").style.display = "block";
+}
+
+function showLoginForm() {
+  document.getElementById("login-form").style.display = "block";
+  document.getElementById("sign-up-form").style.display = "none";
+}
