7a4954462ae0badd7c61c522e9e1a427a703a106
brk-cn <brkcndev@gmail.com>
2024-01-16 17:31:13 +0300

    sign up

--- sign-up.php
+++ sign-up.php
@@ -1,15 +1,13 @@
 <?php
 require_once("db.php");
 
-echo "test test";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  echo "post";
+
   $email = $_POST["email"];
   $password = $_POST["password"];
 
   $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-  echo "test" . $hashed_password;
+
   $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("ss", $email, $hashed_password);
